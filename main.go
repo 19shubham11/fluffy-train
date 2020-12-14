e67cf1cc85df09ad2a355dbb08b5bbaa20cc5cad
Shubham Chaturvedi <19shubham11@gmail.com>
2020-12-15 00:39:58 +0100

    added default http endpoint

--- main.go
+++ main.go
@@ -7,13 +7,30 @@ import (
 
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from snippetbox!"))
 }
+
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("show a snippet!"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("create a snippet!"))
+}
+
 
 func main() {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :2021")
 	err := http.ListenAndServe(":2021", mux)
