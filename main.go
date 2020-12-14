476318122ac8711aadb92f17c9cd39521902faae
Shubham Chaturvedi <19shubham11@gmail.com>
2020-12-15 00:56:21 +0100

    add POST route

--- main.go
+++ main.go
@@ -21,6 +21,13 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("create a snippet!"))
 }
 
