a3c2231f307cc4eb4adc56b3453cd728aa299e61
Shubham Chaturvedi <19shubham11@gmail.com>
2020-12-15 01:20:20 +0100

    added query param

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 
@@ -17,7 +19,14 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("show a snippet!"))
+	query := r.URL.Query().Get("id")
+	id, err := strconv.Atoi(query)
+
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Display a specific snippet with id %d", id)
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
