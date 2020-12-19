b1330c9ed909c086847693f4cd719ec96f8f3978
Shubham Chaturvedi <19shubham11@gmail.com>
2020-12-19 01:29:27 +0100

    added log levels

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,37 +3,38 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
     "net/http"
     "strconv"   
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func(app *application) home(w http.ResponseWriter, r *http.Request) {
     if r.URL.Path != "/" {
         http.NotFound(w, r)
         return
 	}
     
     templateFiles := []string {
-        "../../ui/html/home.page.tmpl",
-        "../../ui/html/base.layout.tmpl",
-        "../../ui/html/footer.partial.tmpl",
+        "./ui/html/home.page.tmpl",
+        "./ui/html/base.layout.tmpl",
+        "./ui/html/footer.partial.tmpl",
     }
 
 	ts, parseErr := template.ParseFiles(templateFiles...)
 	if parseErr != nil {
-		log.Println("template parse error", parseErr.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.errorLog.Println("template parse error", parseErr.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
 	}
 
 	templateErr := ts.Execute(w, nil)
 	if templateErr != nil {
-		log.Println("template err", parseErr.Error())
-		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		app.errorLog.Println("template err", parseErr.Error())
+        http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+        return
 	}
 }
 
-func showSnippet(w http.ResponseWriter, r *http.Request) {
+func(app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
     id, err := strconv.Atoi(r.URL.Query().Get("id"))
     if err != nil || id < 1 {
         http.NotFound(w, r)
@@ -43,7 +44,7 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
     fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
-func createSnippet(w http.ResponseWriter, r *http.Request) {
+func(app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
     if r.Method != http.MethodPost {
         w.Header().Set("Allow", http.MethodPost)
         http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
