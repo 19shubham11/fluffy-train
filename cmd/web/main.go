6bc831f36b1d9b68a8a681f43533e982d076fc87
Shubham Chaturvedi <19shubham11@gmail.com>
2020-12-20 01:33:44 +0100

    setup mysql

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
+	"fmt"
+	"github.com/19shubham11/snippetbox/pkg/models/mysql"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 	"os"
@@ -10,6 +14,7 @@ import (
 type application struct {
 	errorLog *log.Logger
 	infoLog  *log.Logger
+	snippets *mysql.SnippetModel
 }
 
 func main() {
@@ -17,12 +22,24 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	flag.Parse()
 
+	mysqlUsername := os.Getenv("MYSQLUSER")
+	mysqlPassword := os.Getenv("MYSQLPASS")
+
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	dsn := fmt.Sprintf("%s:%s@/snippetbox?parseTime=true", mysqlUsername, mysqlPassword)
+	db, dbErr := openDB(dsn)
+	if dbErr != nil {
+		errorLog.Fatalf("failed to get mysql pools %v", dbErr)
+	}
+
+	defer db.Close()
 
 	app := &application{
 		errorLog: errorLog,
 		infoLog:  infoLog,
+		snippets: &mysql.SnippetModel{DB: db},
 	}
 
 	server := &http.Server{
@@ -34,4 +51,17 @@ func main() {
 	infoLog.Println("Starting server on", *addr)
 	err := server.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/19shubham11/snippetbox
 
 go 1.15
+
+require github.com/go-sql-driver/mysql v1.5.0
